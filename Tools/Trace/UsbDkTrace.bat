6840a5667874d5dfc897fe6cb5aa365e272ede2d
Dmitry Fleytman <dfleytma@redhat.com>
2013-12-20 12:49:14 +0200

    Introduce simple script for WPP tracing
    Signed-off-by: Dmitry Fleytman <dfleytma@redhat.com>

---
+++ Tools/Trace/UsbDkTrace.bat
@@ -0,0 +1,9 @@
+traceview.exe -start UsbDk -rt -flag 0xFF -level 6 -ft 1 -pdb UsbDk.pdb -guid UsbDk.ctl
+
+mkdir WPP
+start traceview.exe -process -rt UsbDk -display -ods -o WPP\\UsbDk.txt -pdb UsbDk.pdb -guid UsbDk.ctl
+start dbgview.exe
+
+pause
+
+traceview.exe -stop UsbDk

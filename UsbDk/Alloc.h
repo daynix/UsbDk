88fffa2179b2725b2608cc88933adbbd56572f72
Dmitry Fleytman <dfleytma@redhat.com>
2013-12-23 11:52:33 +0200

    UsbDk: introduce allocator class
    Signed-off-by: Dmitry Fleytman <dfleytma@redhat.com>

---
+++ UsbDk/Alloc.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <ntddk.h>
+
+template <POOL_TYPE PoolType, ULONG Tag>
+class CAllocatable
+{
+public:
+    void* operator new(size_t /* size */, void *ptr) throw()
+        { return ptr; }
+
+    void* operator new[](size_t /* size */, void *ptr) throw()
+        { return ptr; }
+
+    void* operator new(size_t Size) throw()
+        { return ExAllocatePoolWithTag(PoolType, Size, Tag); }
+
+    void* operator new[](size_t Size) throw()
+        { return ExAllocatePoolWithTag(PoolType, Size, Tag); }
+
+    void operator delete(void *ptr)
+        { ExFreePoolWithTag(ptr, Tag); }
+
+    void operator delete[](void *ptr)
+        { ExFreePoolWithTag(ptr, Tag); }
+
+protected:
+    CAllocatable() {};
+    ~CAllocatable() {};
+};

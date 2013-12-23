a49a62b636bc67b6d4fe8597c50dc196a74d0610
Dmitry Fleytman <dfleytma@redhat.com>
2013-12-23 11:53:37 +0200

    UsbDk: introduce WDF workitem class
    Signed-off-by: Dmitry Fleytman <dfleytma@redhat.com>

---
+++ UsbDk/WdfWorkitem.cpp
@@ -0,0 +1,46 @@
+#include "WdfWorkitem.h"
+
+CWdfWorkitem::~CWdfWorkitem()
+{
+    if (m_hWorkItem != WDF_NO_HANDLE)
+    {
+        WdfObjectDelete(m_hWorkItem);
+    }
+}
+
+typedef struct tag_WdfWorkItemContext
+{
+    CWdfWorkitem* workItem;
+} WdfWorkItemContext;
+
+WDF_DECLARE_CONTEXT_TYPE_WITH_NAME(WdfWorkItemContext, GetWdfWorkItemContext);
+
+VOID CWdfWorkitem::Callback(_In_ WDFWORKITEM WorkItem)
+{
+    auto* ctx = GetWdfWorkItemContext(WorkItem);
+    ctx->workItem->m_Payload(ctx->workItem->m_PayloadCtx);
+}
+
+NTSTATUS CWdfWorkitem::Create()
+{
+    WDF_OBJECT_ATTRIBUTES  attributes;
+    WDF_WORKITEM_CONFIG  workitemConfig;
+
+    WDF_OBJECT_ATTRIBUTES_INIT(&attributes);
+    WDF_OBJECT_ATTRIBUTES_SET_CONTEXT_TYPE(&attributes, WdfWorkItemContext);
+    attributes.ParentObject = m_hParent;
+    WDF_WORKITEM_CONFIG_INIT(&workitemConfig, Callback);
+
+    auto status = WdfWorkItemCreate(&workitemConfig, &attributes, &m_hWorkItem);
+    auto ctx = GetWdfWorkItemContext(m_hWorkItem);
+    ctx->workItem = this;
+
+    return status;
+}
+
+VOID CWdfWorkitem::Enqueue()
+{
+    ASSERT(m_hWorkItem != WDF_NO_HANDLE);
+
+    WdfWorkItemEnqueue(m_hWorkItem);
+}
---
+++ UsbDk/WdfWorkitem.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <ntddk.h>
+#include <wdf.h>
+
+#include "Alloc.h"
+
+class CWdfWorkitem : public CAllocatable<NonPagedPool, 'IWHR'>
+{
+private:
+    typedef VOID(*PayloadFunc)(PVOID Ctx);
+
+public:
+    CWdfWorkitem(_In_ WDFOBJECT parent, PayloadFunc payload, PVOID payloadCtx)
+        : m_hParent(parent)
+        , m_Payload(payload)
+        , m_PayloadCtx(payloadCtx)
+    {}
+
+    CWdfWorkitem(const CWdfWorkitem &other) = delete;
+    CWdfWorkitem& CWdfWorkitem::operator= (const CWdfWorkitem&) = delete;
+
+    ~CWdfWorkitem();
+
+    NTSTATUS Create();
+    VOID Enqueue();
+
+private:
+    WDFWORKITEM m_hWorkItem = WDF_NO_HANDLE;
+    WDFOBJECT m_hParent = WDF_NO_HANDLE;
+    static VOID Callback(_In_  WDFWORKITEM WorkItem);
+
+    PayloadFunc m_Payload;
+    PVOID m_PayloadCtx;
+};

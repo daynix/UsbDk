46a78dc96b87bd7c9009175e7d69179afa80229d
Dmitry Fleytman <dfleytma@redhat.com>
2014-01-12 15:04:50 +0200

    UsbDk: Use CDeviceAccess logic for USB hub identification
    Signed-off-by: Dmitry Fleytman <dfleytma@redhat.com>

--- UsbDk/Filter.cpp
+++ UsbDk/Filter.cpp
@@ -20,63 +20,20 @@ extern "C"
     #include <usbioctl.h>
 }
 
-static BOOLEAN
-UsbDkIsRootHubHwId(_In_ PCWCHAR hwID)
-{
-    PAGED_CODE();
-
-    UNICODE_STRING uHwID;
-    UNICODE_STRING uRH;
-    UNICODE_STRING uRH20;
-
-    RtlInitUnicodeString(&uHwID, hwID);
-    RtlInitUnicodeString(&uRH, L"USB\\ROOT_HUB");
-    RtlInitUnicodeString(&uRH20, L"USB\\ROOT_HUB20");
-
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_FILTER, "%!FUNC! HW ID: %wZ", &uHwID);
-
-    return RtlEqualUnicodeString(&uHwID, &uRH, TRUE) ||
-           RtlEqualUnicodeString(&uHwID, &uRH20, TRUE);
-}
-
-static BOOLEAN
-UsbDkIsRootHub(_In_ PCWCHAR hwIDs, _In_ SIZE_T hwIDsSizeBytes)
-{
-    PAGED_CODE();
-
-    SIZE_T currPos = 0;
-    SIZE_T idLenBytes = 0;
-
-    do
-    {
-        if (!NT_SUCCESS(RtlStringCbLengthW(&hwIDs[currPos], hwIDsSizeBytes, &idLenBytes)))
-        {
-            return FALSE;
-        }
-
-        if (UsbDkIsRootHubHwId(&hwIDs[currPos]))
-        {
-            return TRUE;
-        }
-
-        currPos += idLenBytes / sizeof(WCHAR) + 1;
-    } while (idLenBytes > 0);
-
-    return FALSE;
-}
-
 BOOLEAN
 UsbDkShouldAttach(_In_ WDFDEVICE device)
 {
     PAGED_CODE();
 
     CObjHolder<CDeviceAccess> devAccess(CDeviceAccess::GetDeviceAccess(device));
-    if (devAccess != NULL)
+    if (devAccess)
     {
-        CObjHolder<CMemoryBuffer> hwIDs(devAccess->GetHardwareId());
-        if (hwIDs != NULL)
+        CObjHolder<CRegText> hwIDs(devAccess->GetHardwareIdProperty());
+        if (hwIDs)
         {
-            return UsbDkIsRootHub(static_cast<PCWCHAR>(hwIDs->Ptr()), hwIDs->Size());
+            hwIDs->Dump();
+            return hwIDs->Match(L"USB\\ROOT_HUB") ||
+                   hwIDs->Match(L"USB\\ROOT_HUB20");
         }
     }
 

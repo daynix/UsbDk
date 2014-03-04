395491d18d7ce76964155a0f4e420a145bc4cd4a
Pavel Gurvich <pavel@daynix.com>
2014-03-04 15:03:10 +0200

    UsbDk: Introduce class for DeviceIoControl IRPs
    class CIoControlIrp added
    
    Signed-off-by: Pavel Gurvich <pavel@daynix.com>
    Signed-off-by: Dmitry Fleytman <dfleytma@redhat.com>

--- UsbDk/Irp.h
+++ UsbDk/Irp.h
@@ -2,7 +2,7 @@
 
 #include "ntddk.h"
 #include "Alloc.h"
-#include "MemoryBuffer.h"
+#include "UsbDkUtil.h"
 
 class CIrp : public CAllocatable<NonPagedPool, 'RIHR'>
 {
@@ -33,3 +33,31 @@ private:
     PIRP m_Irp = nullptr;
     PDEVICE_OBJECT m_TargetDevice = nullptr;
 };
+//-------------------------------------------------------------------------------------------------
+
+class CIoControlIrp : public CAllocatable<NonPagedPool, 'CIHR'>
+{
+public:
+    CIoControlIrp() {}
+    ~CIoControlIrp();
+
+    NTSTATUS Create(PDEVICE_OBJECT TargetDevice,
+                    ULONG IoControlCode,
+                    bool IsInternal = true,
+                    PVOID InputBuffer = nullptr,
+                    ULONG InputBufferLength = 0,
+                    PVOID OutputBuffer = nullptr,
+                    ULONG OutputBufferLength = 0);
+    void Destroy();
+    NTSTATUS SendSynchronously();
+
+    CIoControlIrp(const CIoControlIrp&) = delete;
+    CIoControlIrp& operator= (const CIoControlIrp&) = delete;
+
+private:
+    PIRP m_Irp = nullptr;
+    PDEVICE_OBJECT m_TargetDevice = nullptr;
+    CWdmEvent m_Event;
+    IO_STATUS_BLOCK m_IoControlStatus;
+};
+//-------------------------------------------------------------------------------------------------

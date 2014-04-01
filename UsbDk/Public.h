d65b779fee2ebc70b50af78acabf288fe1ad8a1d
Dmitry Fleytman <dfleytma@redhat.com>
2014-04-01 11:54:10 +0300

    UsbDk: Move device names to a separate file
    Move name strings definitions into a separate header
    file to avoid unnecessary interdependencies while
    accessing those strings from driver
    
    Signed-off-by: Dmitry Fleytman <dfleytma@redhat.com>

--- UsbDk/Public.h
+++ UsbDk/Public.h
@@ -11,13 +11,7 @@
 #pragma once
 
 #include "UsbDkData.h"
-
-#define USBDK_DRIVER_NAME       TEXT("UsbDk")
-#define USBDK_DRIVER_FILE_NAME  USBDK_DRIVER_NAME TEXT(".sys")
-#define USBDK_DRIVER_INF_NAME   USBDK_DRIVER_NAME TEXT(".inf")
-#define USBDK_DEVICE_NAME       TEXT("\\Device\\") USBDK_DRIVER_NAME
-#define USBDK_DOSDEVICE_NAME    TEXT("\\DosDevices\\") USBDK_DRIVER_NAME
-#define USBDK_USERMODE_NAME     TEXT("\\\\.\\") USBDK_DRIVER_NAME
+#include "UsbDkNames.h"
 
 #define USBDK_DEVICE_TYPE 50000
 
---
+++ UsbDk/UsbDkNames.h
@@ -0,0 +1,18 @@
+/*
+* UsbDk filter/redirector driver
+*
+* Copyright (c) 2013  Red Hat, Inc.
+*
+* Authors:
+*  Dmitry Fleytman  <dfleytma@redhat.com>
+*
+*/
+
+#pragma once
+
+#define USBDK_DRIVER_NAME       TEXT("UsbDk")
+#define USBDK_DRIVER_FILE_NAME  USBDK_DRIVER_NAME TEXT(".sys")
+#define USBDK_DRIVER_INF_NAME   USBDK_DRIVER_NAME TEXT(".inf")
+#define USBDK_DEVICE_NAME       TEXT("\\Device\\") USBDK_DRIVER_NAME
+#define USBDK_DOSDEVICE_NAME    TEXT("\\DosDevices\\") USBDK_DRIVER_NAME
+#define USBDK_USERMODE_NAME     TEXT("\\\\.\\") USBDK_DRIVER_NAME

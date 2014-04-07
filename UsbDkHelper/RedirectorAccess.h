15035028585ed1840c6ea9dbf9ebb68650d2b5ce
Pavel Gurvich <pavel@daynix.com>
2014-04-07 14:08:42 +0300

    RHHelper: Introduce UsbDkRedirectorAccess class
    UsbDkRedirectorAccess is responsible for redirected device interfacing
    
    Signed-off-by: Pavel Gurvich <pavel@daynix.com>
    Signed-off-by: Dmitry Fleytman <dfleytma@redhat.com>

---
+++ UsbDkHelper/RedirectorAccess.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "UsbDkData.h"
+#include "DriverFile.h"
+#include "UsbDkNames.h"
+
+class UsbDkDriverAccess;
+//-----------------------------------------------------------------------------------
+#define REDIRECTOR_ACCESS_EXCEPTION_STRING TEXT("Redirection operation error. ")
+
+class UsbDkRedirectorAccessException : public UsbDkW32ErrorException
+{
+public:
+    UsbDkRedirectorAccessException() : UsbDkW32ErrorException(REDIRECTOR_ACCESS_EXCEPTION_STRING){}
+    UsbDkRedirectorAccessException(LPCTSTR lpzMessage) : UsbDkW32ErrorException(tstring(REDIRECTOR_ACCESS_EXCEPTION_STRING) + lpzMessage){}
+    UsbDkRedirectorAccessException(LPCTSTR lpzMessage, DWORD dwErrorCode) : UsbDkW32ErrorException(tstring(REDIRECTOR_ACCESS_EXCEPTION_STRING) + lpzMessage, dwErrorCode){}
+    UsbDkRedirectorAccessException(tstring errMsg) : UsbDkW32ErrorException(tstring(REDIRECTOR_ACCESS_EXCEPTION_STRING) + errMsg){}
+    UsbDkRedirectorAccessException(tstring errMsg, DWORD dwErrorCode) : UsbDkW32ErrorException(tstring(REDIRECTOR_ACCESS_EXCEPTION_STRING) + errMsg, dwErrorCode){}
+};
+//-----------------------------------------------------------------------------------
+class UsbDkRedirectorAccess : public UsbDkDriverFile
+{
+public:
+    UsbDkRedirectorAccess()
+        :UsbDkDriverFile(USBDK_TEMP_REDIRECTOR_USERMODE_NAME)
+    {}
+};
+//-----------------------------------------------------------------------------------

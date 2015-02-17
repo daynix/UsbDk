cb5e0a578b8918fe833f39d18735f989b5069ef3
Dmitry Fleytman <dfleytma@redhat.com>
2015-02-17 16:12:47 +0200

    Build: Add package version to MSI name
    Signed-off-by: Dmitry Fleytman <dfleytma@redhat.com>

--- Tools/Installer/buildmsi.bat
+++ Tools/Installer/buildmsi.bat
@@ -1,7 +1,7 @@
 SETLOCAL EnableExtensions EnableDelayedExpansion
 
-IF [%UsbDkVersion%] == [] SET UsbDkVersion="99.99.99"
-IF [%UsbDkVersion%] == [".."] SET UsbDkVersion="99.99.99"
+IF [%UsbDkVersion%] == [] SET UsbDkVersion=99.99.99
+IF [%UsbDkVersion%] == [".."] SET UsbDkVersion=99.99.99
 
 pushd ..\..\Install_Debug\x86
 
@@ -9,29 +9,29 @@ del UsbDk_Debug_x86.msi UsbDk_Debug_x86.wixobj UsbDk_Debug_x86.wixpdb
 
 "C:\Program Files (x86)\WiX Toolset v3.8\bin\candle.exe" ..\..\Tools\Installer\UsbDkInstaller.wxs -out UsbDk_Debug.wixobj -dUsbDkVersion=%UsbDkVersion% -dConfig=Debug
 if !ERRORLEVEL! NEQ 0 exit /B 1
-"C:\Program Files (x86)\WiX Toolset v3.8\bin\light.exe" UsbDk_Debug.wixobj -out UsbDk_Debug_x86.msi -sw1076
+"C:\Program Files (x86)\WiX Toolset v3.8\bin\light.exe" UsbDk_Debug.wixobj -out UsbDk_Debug_%UsbDkVersion%_x86.msi -sw1076
 if !ERRORLEVEL! NEQ 0 exit /B 1
 
 popd
 
 pushd ..\..\Install_Debug\x64
 
-del UsbDk_Debug_x64.msi UsbDk_Debug_x64.msi.wixobj UsbDk_Debug_x64.msi.wixpdb
+del *.msi *.wixobj *.wixpdb
 
 "C:\Program Files (x86)\WiX Toolset v3.8\bin\candle.exe" ..\..\Tools\Installer\UsbDkInstaller.wxs -out UsbDk_Debug.wixobj -dUsbDkVersion=%UsbDkVersion% -dConfig=Debug -dUsbDk64Bit=1
 if !ERRORLEVEL! NEQ 0 exit /B 1
-"C:\Program Files (x86)\WiX Toolset v3.8\bin\light.exe" UsbDk_Debug.wixobj -out UsbDk_Debug_x64.msi -sw1076
+"C:\Program Files (x86)\WiX Toolset v3.8\bin\light.exe" UsbDk_Debug.wixobj -out UsbDk_Debug_%UsbDkVersion%_x64.msi -sw1076
 if !ERRORLEVEL! NEQ 0 exit /B 1
 
 popd
 
 pushd ..\..\Install\x86
 
-del UsbDk_x86.msi UsbDk_x86.wixobj UsbDk_x86.wixpdb
+del *.msi *.wixobj *.wixpdb
 
 "C:\Program Files (x86)\WiX Toolset v3.8\bin\candle.exe" ..\..\Tools\Installer\UsbDkInstaller.wxs -out UsbDk.wixobj -dUsbDkVersion=%UsbDkVersion% -dConfig=Release
 if !ERRORLEVEL! NEQ 0 exit /B 1
-"C:\Program Files (x86)\WiX Toolset v3.8\bin\light.exe" UsbDk.wixobj -out UsbDk_x86.msi -sw1076
+"C:\Program Files (x86)\WiX Toolset v3.8\bin\light.exe" UsbDk.wixobj -out UsbDk_%UsbDkVersion%_x86.msi -sw1076
 if !ERRORLEVEL! NEQ 0 exit /B 1
 
 popd
@@ -42,7 +42,7 @@ del UsbDk_x64.msi UsbDk_x64.msi.wixobj UsbDk_x64.msi.wixpdb
 
 "C:\Program Files (x86)\WiX Toolset v3.8\bin\candle.exe" ..\..\Tools\Installer\UsbDkInstaller.wxs -out UsbDk.wixobj -dUsbDkVersion=%UsbDkVersion% -dConfig=Release -dUsbDk64Bit=1
 if !ERRORLEVEL! NEQ 0 exit /B 1
-"C:\Program Files (x86)\WiX Toolset v3.8\bin\light.exe" UsbDk.wixobj -out UsbDk_x64.msi -sw1076
+"C:\Program Files (x86)\WiX Toolset v3.8\bin\light.exe" UsbDk.wixobj -out UsbDk_%UsbDkVersion%_x64.msi -sw1076
 if !ERRORLEVEL! NEQ 0 exit /B 1
 
 popd

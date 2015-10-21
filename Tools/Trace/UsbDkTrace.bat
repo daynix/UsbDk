78f77172618740fee1d9ba37bc55c9ef5d58b2e1
Dmitry Fleytman <dfleytma@redhat.com>
2015-10-21 18:03:43 +0300

    Trace: Enable all trace flags by default
    Signed-off-by: Dmitry Fleytman <dfleytma@redhat.com>

--- Tools/Trace/UsbDkTrace.bat
+++ Tools/Trace/UsbDkTrace.bat
@@ -1,6 +1,6 @@
 @echo off
 
-traceview.exe -start UsbDk -rt -flag 0xFF -level 6 -ft 1 -pdb UsbDk.pdb -guid UsbDk.ctl
+traceview.exe -start UsbDk -rt -flag 0x7FFFFFFF -level 6 -ft 1 -pdb UsbDk.pdb -guid UsbDk.ctl
 
 mkdir WPP
 start traceview.exe -process -rt UsbDk -display -ods -o WPP\\UsbDk.txt -pdb UsbDk.pdb -guid UsbDk.ctl
